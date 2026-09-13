548e82387127f633afdeb3e244f57b33df78353b
Анна <anncao.rus@gmail.com>
2026-09-13 23:26:00 +0400

    Лабораторная 1: основы синтаксиса Go

---
+++ lab1/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Задание 5: сумма и разность двух чисел с плавающей запятой
+func sumAndDiff(a, b float64) (float64, float64) {
+	return a + b, a - b
+}
+
+// Задание 6: среднее значение трёх чисел
+func average(a, b, c float64) float64 {
+	return (a + b + c) / 3
+}
+
+func main() {
+	// Задание 1: текущее время и дата
+	now := time.Now()
+	fmt.Println("Текущая дата и время:", now.Format("2006-01-02 15:04:05"))
+
+	// Задание 2: переменные различных типов
+	var i int = 42
+	var f float64 = 3.14
+	var s string = "Hello, Go!"
+	var b bool = true
+	fmt.Printf("int: %d, float64: %.2f, string: %s, bool: %t\n", i, f, s, b)
+
+	// Задание 3: краткая форма объявления переменных
+	x := 10
+	y := 2.5
+	name := "Gopher"
+	flag := false
+	fmt.Printf("x=%d, y=%.1f, name=%s, flag=%t\n", x, y, name, flag)
+
+	// Задание 4: арифметические операции
+	a, b2 := 15, 4
+	fmt.Printf("%d + %d = %d\n", a, b2, a+b2)
+	fmt.Printf("%d - %d = %d\n", a, b2, a-b2)
+	fmt.Printf("%d * %d = %d\n", a, b2, a*b2)
+	fmt.Printf("%d / %d = %d\n", a, b2, a/b2)
+	fmt.Printf("%d %% %d = %d\n", a, b2, a%b2)
+
+	// Задание 5
+	sum, diff := sumAndDiff(7.5, 2.5)
+	fmt.Printf("Сумма: %.2f, Разность: %.2f\n", sum, diff)
+
+	// Задание 6
+	fmt.Printf("Среднее значение: %.2f\n", average(3, 7, 11))
+}
